f8fa661f000904881e40b4f35309710e8b5c886c
Afron Orana <afron.orana@gmail.com>
2023-06-06 16:18:42 +0200

    fix: change style on detail view

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -2,20 +2,13 @@
   <PanelItem :index="index" :field="field">
     <template #value>
       <div v-if="field.value.length > 0">
-        <TagGroup
-          v-if="field.style === 'group'"
-          :tags="field.value"
-          :resource-name="field.resourceName"
-          :editable="false"
-          :with-preview="field.withPreview"
-        />
-        <TagList
-          v-if="field.style === 'list'"
-          :tags="field.value"
-          :resource-name="field.resourceName"
-          :editable="false"
-          :with-preview="field.withPreview"
-        />
+        <TagGroup v-if="field.style === 'group'" :tags="field.value" :resource-name="field.resourceName" :editable="false"
+          :with-preview="field.withPreview" />
+        <div v-if="field.style === 'list'">
+          <ul class="list-disc">
+            <li class="mb-2" v-for="item in field.value" :key="item.display">{{ item.display }}</li>
+          </ul>
+        </div>
       </div>
     </template>
   </PanelItem>
@@ -26,3 +19,9 @@ export default {
   props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
 }
 </script>
+
+<style scoped>
+.list-disc {
+  list-style-type: disc;
+}
+</style>
